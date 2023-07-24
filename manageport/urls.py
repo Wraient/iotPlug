11d6691de7d01841bdb61df98ba000ace1c8c841
Wraient Daemon <wraient.911@gmail.com>
2023-07-25 00:32:46 +0530

    button works

--- manageport/urls.py
+++ manageport/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("port/<int:id>", views.access),
-    path("", views.render_manageport),
-    path("<str:string>", views.render_404) 
+    path("", views.render_manageport, name="manageport"),
+    path("<str:string>", views.render_404),
 ]
--- manageport/views.py
+++ manageport/views.py
@@ -3,17 +3,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# data = {"1": True, "2": False, "3": False, "4": False, "5": False, "6": False, "7": False, "8": False, "9": False, "10": False}
 data = {"1": True}
+
+
+
 
 def access(request, id):
     return HttpResponse(id)
 
 def render_manageport(request):
-
+    if request.method == 'POST':
+        print('es')
+    
+    # print("no")
+    # print(request)
     return render(request, 'manageport/index.html', {
         "data_list":data
     })
-
 
 def render_404(request, string):
     return HttpResponse("<h1>error 404<h1>")
