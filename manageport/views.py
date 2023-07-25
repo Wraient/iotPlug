865fbd57c17d6eb3ebb265f1d74349bff7dc41b3
Wraient Daemon <wraient.911@gmail.com>
2023-07-25 14:47:08 +0530

    checkpoint!

--- manageport/views.py
+++ manageport/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
 # Create your views here.
 
 
-# data = {"1": True, "2": False, "3": False, "4": False, "5": False, "6": False, "7": False, "8": False, "9": False, "10": False}
-data = {"1": True}
+data = {"1": True, "2": False, "3": False, "4": False, "5": False, "6": False, "7": False, "8": False, "9": False, "10": False}
+# data = {"1": True, }
 
 
-
+def port_update(button_value):
+    print(button_value)
+    given_data = button_value.split()
+    action = given_data[0]
+    key = given_data[1]
+    if action == "off": 
+        action = False
+    else: 
+        action = True
+    data[key] = action
+    print(data)
+    print(key, action)
 
 def access(request, id):
     return HttpResponse(id)
 
 def render_manageport(request):
+    print(request)
     if request.method == 'POST':
-        print('es')
+        print('clicked')
         button_value = request.POST.get('port_update')
-        print(button_value)
+        port_update(button_value)
+        # return redirect("/manageport/#"+button_value.split()[1])
+    print(data)
+
+        # print(button_value)
     
     # print("no")
     # print(request)
